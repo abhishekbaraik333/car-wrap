c7aa71dd79c316b8a61de3c1f572922f471918f6
abhishekbaraik333 <abhitamangme@gmail.com>
2025-05-14 15:58:39 +0530

    added wraps section

---
+++ script.js
@@ -0,0 +1,31 @@
+
+  const swiper = new Swiper('.mySwiper', {
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+    }
+  });
